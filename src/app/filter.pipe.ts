06262eed5e878f63a85c2aff426bb7795a91ab44
Justina Kuchta <justyna.kuchta@gmail.com>
2020-07-08 16:29:18 -0400

    added async pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  // pipe gets recalculated every time the data changes which may lead to performance issues
+  // forces update on each data change
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, filterString: string, propName: string): any {
-    if (value.length === 0) return value
+  transform(value: any, filterString: string): any {
+    if (value.length === 0 || filterString === '') return value
     let resultArr = []
     for (let item of value) {
       if (item.status === filterString) resultArr.push(item)
